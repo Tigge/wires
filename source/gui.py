5e7cb25542924ffe0be253f34f1223742be7d1c5
tiggex <tiggex@cc3707a3-7b35-2dd2-58da-d225956214ce>
2010-08-15 09:47:42 +0000

    Added FPS counter
    git-svn-id: https://wires.googlecode.com/svn/trunk@5 cc3707a3-7b35-2dd2-58da-d225956214ce

--- source/gui.py
+++ source/gui.py
@@ -51,14 +51,22 @@ comps = [s1, s2, ac, light]
 
 guicomps = [ComponentGUI(s1), ComponentGUI(s2), ComponentGUI(ac), ComponentGUI(light)]
 
+
+font = pygame.font.Font(pygame.font.get_default_font(), 16)
+fps  = pygame.time.Clock()
 while 1:
+    fps.tick()
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
     screen.fill(black)
+
+    fs = font.render("FPS: " + str(int(fps.get_fps())), True, (255, 255, 255))
+    screen.blit(fs, (10, 10))
 
     for i, guicomp in enumerate(guicomps):
         screen.blit(guicomp._surface, (50 + 100 * i, 100))
     
     pygame.display.flip()
 
+    
