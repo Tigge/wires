7ce613763cfae37306329b12062628ea268a8d3e
tiggex <tiggex@cc3707a3-7b35-2dd2-58da-d225956214ce>
2010-08-17 20:57:54 +0000

    Testing wires
    git-svn-id: https://wires.googlecode.com/svn/trunk@10 cc3707a3-7b35-2dd2-58da-d225956214ce

--- source/gui/component.py
+++ source/gui/component.py
@@ -27,6 +27,8 @@ class MovableComponent(base.Component, event.MouseListener):
 
 class ComponentGUI(MovableComponent):
 
+    _mapping      = {}
+
     _color_base   = 66, 66, 66
     _color_frame  = 150, 150, 150
     _color_input  = 255, 0, 0
@@ -35,6 +37,8 @@ class ComponentGUI(MovableComponent):
     def __init__(self, comp):
         MovableComponent.__init__(self)
         self._component = comp
+        for inpt in comp.inputs:
+            ComponentGUI._mapping[inpt] = self
         
     def paint(self, surface):
         rect = pygame.Rect(self._position, self._dimension)
@@ -48,4 +52,17 @@ class ComponentGUI(MovableComponent):
         for i, outp in enumerate(self._component.outputs):
             rect = pygame.Rect((x + w - 15, y + 5 + i * 15), (10, 10))
             pygame.gfxdraw.box(surface, rect, ComponentGUI._color_output)
-
+        self.paintwires(surface)
+    
+    def paintwires(self, surface):
+        for i, outp in enumerate(self._component.outputs):
+            if outp == None:
+                return
+            for inpt in outp._inputs:
+                if inpt in ComponentGUI._mapping:
+                    other = ComponentGUI._mapping[inpt]
+                    pygame.gfxdraw.line(surface, self._position[0], self._position[1], \
+                        other._position[0], other._position[1], (255, 255, 255))
+                    
+                    
+                
